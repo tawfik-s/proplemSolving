16dd91137af69f466347db922003fb56e83b3617
tawfik <t.shalash1@gmail.com>
2021-07-25 13:59:54 +0200

    stls

--- stl/main.cpp
+++ stl/main.cpp
@@ -18,14 +18,10 @@ freopen("in.txt","r",stdin);
 freopen("out.txt","w",stdout);
 #endif
 
-vector<int> v;   //empty vector of int
-vector<int> v2(4,100);  //set 4 ints with value 100
-vector<int>v3(v2.begin(),v2.end()); //iterate around v2 to copy data to v3
-vector<int>v4(v3); //copy all data in v3 into v4
 
-int myints[]={1,2,3,4,5};
 
-vector<int>v5(myints,myints+5);
+
+
 
 
 
