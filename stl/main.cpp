14284d6ae1a9d3fc72eafa43c480dd89b3ab2d7d
tawfik <t.shalash1@gmail.com>
2021-07-25 11:00:56 +0200

    stls learn

---
+++ stl/main.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+freopen("in.txt","r",stdin);
+freopen("out.txt","w",stdout);
+#endif
+
+vector<int>v ={1,2,3,4,5};
+
+vector<int>::iterator it= v.begin();
+
+cout<<" iterator point to v.begin() "<<*it<<endl;
+it=next(it,1);
+cout<<" iterator after next(it,1) = "<<*it<<endl;
+it=prev(it,1);
+cout<<" iterator after prev(it,1) = "<<*it<<endl;
+advance(it,3);
+cout<<" iterator after advance(it,3)= "<<*it<<endl;
+
+
+
+
+
+// for(vector<int>::iterator it=v.begin();it!=v.end();it++)   //iterate arount the elments using iterator
+// {
+//     cout<<*it<<endl;
+// }
+
+
+    return 0;
+}
+
+
+
+
+//pointer revision
+// int n=5;
+// int *ptr=&n;
+// n=6;
+// cout<<ptr<<endl;
+// cout<<&n<<endl;
+// cout<<*ptr<<endl;
+// cout<<n<<endl;
